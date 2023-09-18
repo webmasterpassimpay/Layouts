e2473f9281f2fa3d95f313003588e2477b5da58d
andreiyaruk <84790529+andreiyaruk@users.noreply.github.com>
2023-09-18 14:45:33 +0300

    index2

--- js/crypto_gambling.js
+++ js/crypto_gambling.js
@@ -1,36 +1,38 @@
 
 // Определение устройства PC / Mobile (mouse / touchScreen)
 
-const isMobile = {
-   Android: function () {
-      return navigator.userAgent.match(/Android/i);
-   },
-   BlackBerry: function () {
-      return navigator.userAgent.match(/BlackBerry/i);
-   },
-   iOS: function () {
-      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-   },
-   Opera: function () {
-      return navigator.userAgent.match(/Opera Mini/i);
-   },
-   Windows: function () {
-      return navigator.userAgent.match(/IEMobile/i);
-   },
-   any: function () {
-      return (
-         isMobile.Android() ||
-         isMobile.BlackBerry() ||
-         isMobile.iOS() ||
-         isMobile.Opera() ||
-         isMobile.Windows());
-   }
-};
 
-if (isMobile.any()) {
-   document.body.classList.add('_touch');
-} else {
-   document.body.classList.add('_pc');
+if (!isMobile) {
+   const isMobile = {
+      Android: function () {
+         return navigator.userAgent.match(/Android/i);
+      },
+      BlackBerry: function () {
+         return navigator.userAgent.match(/BlackBerry/i);
+      },
+      iOS: function () {
+         return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+      },
+      Opera: function () {
+         return navigator.userAgent.match(/Opera Mini/i);
+      },
+      Windows: function () {
+         return navigator.userAgent.match(/IEMobile/i);
+      },
+      any: function () {
+         return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows());
+      }
+   };
+   if (isMobile.any()) {
+      document.body.classList.add('_touch');
+   } else {
+      document.body.classList.add('_pc');
+   };
 };
 
 /* открывает блоки в Questions */
